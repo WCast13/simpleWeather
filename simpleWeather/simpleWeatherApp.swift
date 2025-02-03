a7db602c5cb6bd59c8dfccca67c8fff04c87f102
Will Castellano <wcastellano13@gmail.com>
2025-02-03 16:21:45 -0500

    Initial Commit

---
+++ simpleWeather/simpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleWeatherApp.swift
+//  simpleWeather
+//
+//  Created by William Castellano on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct simpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
